bf70d782e4170bc601f38edc91523c3a967a2a9d
ijuvrk <ijascochin@gmail.com>
2024-11-20 16:03:07 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ijaz Ahmad on 20/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
